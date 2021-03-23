a0999058dfc4559baf18e52b8ee50c1a8968d87e
Cillian Whelan <l00162026@student.lyit.ie>
2021-03-23 08:08:33 +0000

    Setup Finished

--- app/src/main/java/com/example/e_commerce_11/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/e_commerce_11/activities/RegisterActivity.kt
@@ -8,6 +8,7 @@ package com.example.e_commerce_11.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.e_commerce_11.R
@@ -16,7 +17,7 @@ import kotlinx.android.synthetic.main.activity_register.*
 
 
 //register activity allows new members to create an account
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -27,6 +28,11 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        btn_register.setOnClickListener{
+            verifyUserDetails()
+        }
+
     }
 
     private fun setupActionBar(){
@@ -42,5 +48,43 @@ class RegisterActivity : AppCompatActivity() {
         toolbar_register_activity.setNavigationOnClickListener{
             onBackPressed()
             finish()}
+    }
+
+    //this function sets the error message to be displayed if a user does not enter details in a field
+    fun verifyUserDetails() : Boolean{
+        return when{
+            TextUtils.isEmpty(edit_first_name.text.toString().trim{it <= ' '}) -> {
+                displaySnackBar(resources.getString(R.string.error_first_name), true)
+                false
+            }
+            TextUtils.isEmpty(edit_surname.text.toString().trim{it <= ' '}) -> {
+                displaySnackBar(resources.getString(R.string.error_surname), true)
+                false
+            }
+            TextUtils.isEmpty(edit_email.text.toString().trim{it <= ' '}) -> {
+                displaySnackBar(resources.getString(R.string.error_email_address), true)
+                false
+            }
+            TextUtils.isEmpty(edit_password.text.toString().trim{it <= ' '}) -> {
+                displaySnackBar(resources.getString(R.string.error_password), true)
+                false
+            }
+            TextUtils.isEmpty(edit_confirm_password.text.toString().trim{it <= ' '}) -> {
+                displaySnackBar(resources.getString(R.string.error_confirm_password), true)
+                false
+            }
+            edit_password.text.toString().trim{it <= ' '} != edit_confirm_password.text.toString().trim{it <= ' '} -> {
+                displaySnackBar(resources.getString(R.string.error_passwords_don_t_match), true)
+                false
+            }
+            !checkbox_t_and_c.isChecked -> {
+                displaySnackBar(resources.getString(R.string.error_t_and_c_not_checked), true)
+                false
+            }
+            else -> {
+                displaySnackBar(resources.getString(R.string.success), false)
+                true
+            }
+        }
     }
 }
