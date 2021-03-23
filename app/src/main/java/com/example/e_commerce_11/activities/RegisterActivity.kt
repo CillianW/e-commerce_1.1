85bfed43e1efec49aa60bab125e95729d4438d09
Cillian Whelan <l00162026@student.lyit.ie>
2021-03-23 01:21:15 +0000

    Back Button on Register Activity Sorted -> Scroll view was covering the button

--- app/src/main/java/com/example/e_commerce_11/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/e_commerce_11/activities/RegisterActivity.kt
@@ -8,11 +8,12 @@ package com.example.e_commerce_11.activities
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.e_commerce_11.R
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register.*
+
 
 //register activity allows new members to create an account
 class RegisterActivity : AppCompatActivity() {
@@ -26,7 +27,6 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
         }
-
     }
 
     private fun setupActionBar(){
@@ -39,6 +39,8 @@ class RegisterActivity : AppCompatActivity() {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_back_arrow)
         }
 
-        toolbar_register_activity.setNavigationOnClickListener{ onBackPressed() }
+        toolbar_register_activity.setNavigationOnClickListener{
+            onBackPressed()
+            finish()}
     }
 }
