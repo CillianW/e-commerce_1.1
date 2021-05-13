fd3492bbf1b66f505a307f19d8f4a7698a3d59c9
Cillian Whelan <l00162026@student.lyit.ie>
2021-05-13 16:24:43 +0100

    Bugs fixed
    Next step: Display individual Order details

---
+++ app/src/main/java/com/example/e_commerce_11/activities/OrderDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.e_commerce_11.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.e_commerce_11.R
+
+class OrderDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_details)
+    }
+}
